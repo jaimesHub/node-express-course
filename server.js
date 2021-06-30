2da46159bf92416b7e9426a5473726bf1daa4707
System Administrator <root@jaimes-mac.local>
2021-07-01 01:09:29 +0700

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+// In order to make POST requests, first we need to include the "body-parser" library
+app.user(bodyParser.json());
 
 const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
 
@@ -11,6 +15,7 @@ app.get("/users", function (req, res) {
   });
 });
 
+// colons are used as variables that be viewed in the params
 app.get("/users/:id", function (req, res) {
   console.log(req.params.id);
   res.json({
@@ -18,6 +23,28 @@ app.get("/users/:id", function (req, res) {
     message: "Got one user",
     user: req.params.id,
   });
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // This should come from the database
+  const mockUsername = "billyTheKid";
+  const mockPassword = "superSecret";
+
+  if (username === mockUsername && password === mockPassword) {
+    res.json({
+      sucess: true,
+      message: "password and username match!",
+      toke: "encrypted token goes here.",
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "password and username do not match",
+    });
+  }
 });
 
 app.listen(8000, function () {
