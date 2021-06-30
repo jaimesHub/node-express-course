96ae0e2c69507c01f55198a3a14b180517bee32a
System Administrator <root@jaimes-mac.local>
2021-07-01 01:01:02 +0700

    add dynamic GET route

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.get("/users", function (req, res) {
   });
 });
 
+app.get("/users/:id", function (req, res) {
+  console.log(req.params.id);
+  res.json({
+    success: true,
+    message: "Got one user",
+    user: req.params.id,
+  });
+});
+
 app.listen(8000, function () {
   console.log("Server is running...");
 });
